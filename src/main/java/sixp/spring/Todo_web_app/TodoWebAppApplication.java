abc5a162fc98464660a6a059838675a5b193a42b
SixpCoder <117176600+Sixpcoder@users.noreply.github.com>
2023-12-17 19:41:35 +0530

    initial commit

---
+++ src/main/java/sixp/spring/Todo_web_app/TodoWebAppApplication.java
@@ -0,0 +1,13 @@
+package sixp.spring.Todo_web_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sixp/spring/Todo_web_app/TodoWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sixp.spring.Todo_web_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
